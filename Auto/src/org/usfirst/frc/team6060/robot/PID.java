de266dc6ed8a779fa81feb15426e27ff7bae9ec7
Administrator <Administrator@RHSVIDPROTEST2.skusd.local>
2018-01-16 12:33:48 -0800

    fixed bugs in PID class

--- Auto/src/org/usfirst/frc/team6060/robot/PID.java
+++ Auto/src/org/usfirst/frc/team6060/robot/PID.java
@@ -1,10 +1,11 @@
 package org.usfirst.frc.team6060.robot;
-import java.lang.System.currentTimeMillis();
+import java.lang.System.*;
+import java.util.Date;
 public class PID {
     //This will tell the PID loop what to aim for
     public double setpoint = 0;
     //Internal math 
-    unsigned long lastTime = 0;
+     long lastTime = 0;
     private double Input, Output;
     private double P, I, D;
     private double errSum, lastErr;
@@ -14,26 +15,27 @@ public class PID {
     
   void Caluclate() {
     //find what time it is now and how long it has been since the loop was last called
-    unsigned long now = millis();
-    unsigned long timechanged = (now - lastTime);
+     long now = System.currentTimeMillis();
+     long timechanged = (now - lastTime);
     
-    //error varibles
+    //error variables
     double error = (setpoint - lastErr);
-    double errSum += (error * timechanged);
-    double Derr = (error - lastErr)/ Timechanged;
+    double errSum =+ (error * timechanged);
+    double Derr = (error - lastErr)/ timechanged;
     
     //compute output
-    Output = (P * error) + (I * errSum) + (D * Derr)
+    Output = (P * error) + (I * errSum) + (D * Derr);
   }
   
-  // Update the tuning_values of a PID loop using the Tuneing value table
+  // Update the tuning_values of a PID loop using the Tuning value table
     public void tune(double[][] tuneing_table, int whichPID) {
       for(int eachPID = 0; eachPID < 3; eachPID++) {
         tuneing_values[eachPID] = tuneing_table[whichPID][eachPID];
     }
+    }
     
       //update the current setpoint for the PIDloop
-    public void Set_point(double new_set_point) {
+    public void Set_point(double new_setpoint) {
         setpoint = new_setpoint;
     }
       
